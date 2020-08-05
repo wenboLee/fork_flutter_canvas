df2f3ed2fcf38990091ebae81de862c34c645359
xiaocode <337317439@qq.com>
2020-08-05 18:29:24 +0800

    xc

--- lib/pages/anim01/anim0104.dart
+++ lib/pages/anim01/anim0104.dart
@@ -16,8 +16,8 @@ class _Anim0104PageState extends State<Anim0104Page>
     with SingleTickerProviderStateMixin {
   AnimationController _controller;
   Size size;
-  Ball _ball = Ball(x: 50, y: 50, r: 30);
-  double angle = 0, speed = 0, r = 150;
+  Ball _ball = Ball(x: 0, y: 0, r: 30);
+  double angle = 0, speed = 0.01, r = 150;
 
   @override
   void initState() {
@@ -41,16 +41,21 @@ class _Anim0104PageState extends State<Anim0104Page>
 
   @override
   Widget build(BuildContext context) {
-    size = MediaQuery.of(context).size;
+    // 外面传进去size和Canvas接受到的size不一样，iPhone X的height不等812/712
+    // size = MediaQuery.of(context).size;
+    var w = MediaQuery.of(context).size.width;
+    size = Size(w, w);
     return Scaffold(
       appBar: appBar(widget.title),
       body: Container(
         child: AnimatedBuilder(
           animation: _controller,
           builder: (context, child) {
-            return CustomPaint(
-              size: size,
-              painter: MyCustomPainter(ball: _ball, r: r),
+            return Center(
+              child: CustomPaint(
+                size: size,
+                painter: MyCustomPainter(ball: _ball, r: r),
+              ),
             );
           },
         ),
@@ -78,8 +83,10 @@ class MyCustomPainter extends CustomPainter {
     _paint.color = Colors.black;
     canvas.drawCircle(size.center(Offset.zero), r, _paint);
 
-    canvas.drawLine(Offset(0, size.height / 2.0), Offset(size.width, size.height / 2.0), _paint);
-    canvas.drawLine(Offset(size.width / 2.0, 0), Offset(size.width / 2.0, size.height), _paint);
+    canvas.drawLine(Offset(0, size.height / 2.0),
+        Offset(size.width, size.height / 2.0), _paint);
+    canvas.drawLine(Offset(size.width / 2.0, 0),
+        Offset(size.width / 2.0, size.height), _paint);
 
     _paint.style = PaintingStyle.fill;
     _paint.color = Colors.blue;
@@ -88,6 +95,7 @@ class MyCustomPainter extends CustomPainter {
     _paint.color = Colors.red;
     canvas.drawLine(Offset(ball.x, 0), Offset(ball.x, size.height), _paint);
     canvas.drawLine(Offset(0, ball.y), Offset(size.width, ball.y), _paint);
+    canvas.drawLine(size.center(Offset.zero), Offset(ball.x, ball.y), _paint);
     canvas.restore();
   }
 

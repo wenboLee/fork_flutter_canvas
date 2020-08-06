b0166cf1b83454123dc1315174f31bda8eeee6c9
xiaocode <337317439@qq.com>
2020-08-06 10:11:05 +0800

    xc

--- lib/route/main.dart
+++ lib/route/main.dart
@@ -10,10 +10,10 @@ import 'package:flutter_canvas/pages/main.dart';
 class RouteConstant {
   static const String DebugMainPage = '/';
   static const String MainPage = '/main';
-  static const String Anim0101Page = '/anim0101Page';
-  static const String Anim0102Page = '/anim0102Page';
-  static const String Anim0103Page = '/anim0103Page';
-  static const String Anim0104Page = '/anim0104Page';
+  static const String Anim01Page = '/anim01Page';
+  static const String Anim02Page = '/anim02Page';
+  static const String Anim03Page = '/anim03Page';
+  static const String Anim04Page = '/anim04Page';
 }
 
 ///通过 routeName名称 返回 Widget的名称
@@ -22,10 +22,10 @@ String routeWidgetName(String routeName) {
   var map = {
     RouteConstant.DebugMainPage: 'DebugMainPage',
     RouteConstant.MainPage: 'MainPage',
-    RouteConstant.Anim0101Page: 'Anim0101Page',
-    RouteConstant.Anim0102Page: 'Anim0102Page',
-    RouteConstant.Anim0103Page: 'Anim0103Page',
-    RouteConstant.Anim0104Page: 'Anim0104Page',
+    RouteConstant.Anim01Page: 'Anim01Page',
+    RouteConstant.Anim02Page: 'Anim02Page',
+    RouteConstant.Anim03Page: 'Anim03Page',
+    RouteConstant.Anim04Page: 'Anim04Page',
   };
   return map.containsKey(routeName) ? map[routeName] : defaultRouteName;
 }
@@ -35,9 +35,9 @@ Map<String, WidgetBuilder> routes(BuildContext context) {
   return {
     RouteConstant.MainPage: (context) => MainPage(title: 'main'),
     RouteConstant.DebugMainPage: (context) => DebugMainPage(title: 'debug-route'),
-    RouteConstant.Anim0101Page: (context) => Anim01Page(title: '任意方向上的加速度'),
-    RouteConstant.Anim0102Page: (context) => Anim02Page(title: '单轴加速度'),
-    RouteConstant.Anim0103Page: (context) => Anim03Page(title: '单轴运动'),
-    RouteConstant.Anim0104Page: (context) => Anim04Page(title: '圆周运动'),
+    RouteConstant.Anim01Page: (context) => Anim01Page(title: '任意方向上的加速度'),
+    RouteConstant.Anim02Page: (context) => Anim02Page(title: '单轴加速度'),
+    RouteConstant.Anim03Page: (context) => Anim03Page(title: '单轴运动'),
+    RouteConstant.Anim04Page: (context) => Anim04Page(title: '圆周运动'),
   };
 }

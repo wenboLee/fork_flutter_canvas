a65b0b88ae543c4736ffa9939fdb40e20e18d216
yx <>
2020-08-09 21:44:13 +0800

    xc

--- lib/pages/anim/anim09.dart
+++ lib/pages/anim/anim09.dart
@@ -32,7 +32,7 @@ class _Anim09PageState extends State<Anim09Page>
           _size = _globalKey.currentContext.size;
         }
         if (_arrow == null) {
-          _arrow = Arrow(x: _size.width / 2, y: _size.height / 2, w: 60, h: 30);
+          _arrow = Arrow(x: _size.width / 2, y: _size.height / 2, w: 100, h: 60);
         }
         if (_pointer != Offset.zero) {
           dx = _pointer.dx - _arrow.x;
